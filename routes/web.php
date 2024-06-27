fae4659ee8de59d9a95b421aa456f80d725e80d7
karim <karimashraf851@gmail.com>
2024-06-27 09:44:49 +0300

    #

--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,18 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-Route::resource('users', UserController::class);
+// Route::resource('users', UserController::class);
 Route::resource('posts', PostController::class);
 Route::resource('comments', CommentController::class);
 Route::get('settings', function () {
     return view('settings');
 });
-
+Route::middleware('role:SuperAdmin')->group(function () {
+    Route::resource('roles', RoleController::class);
+    Route::post('roles/{role}/permissions', [RoleController::class, 'givePermission'])->name('roles.permissions');
+    Route::delete('roles/{role}/permissions/{permission}', [RoleController::class, 'revokePermission'])->name('roles.permissions.revoke');
+    Route::resource('users', UserController::class);
+});
 
 
 require __DIR__.'/auth.php';
